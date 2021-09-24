ce5bd6826752fdc5247f22261347f589728d07fd
Ravi A <89862619+ravia-10@users.noreply.github.com>
2021-09-24 07:04:35 +0530

    Add files via upload

---
+++ GumStick.java
@@ -0,0 +1,24 @@
+package com.xworkz.oops;
+
+public class GumStick {
+	int price;
+	String color;
+	String name;
+	double height;
+	double weight;
+	String shape;
+	
+	
+	public void openCap(){
+		System.out.println("open the cap");
+	}
+	
+	public void closeCap(){
+		System.out.println("close the Cap");
+	}
+	 
+	public void fillOrUnfill(){
+		System.out.println("fill or unfill the stick");
+	}
+}
+
---
+++ IronBox.java
@@ -0,0 +1,22 @@
+package com.xworkz.oops;
+
+public class IronBox {
+	int price;
+	String brand;
+	String color;
+	String type;
+	double lenght;
+	String design;
+
+	public void switchOn() {
+		System.out.println("it is On");
+	}
+
+	public void switchOff() {
+		System.out.println("it is Off");
+	}
+
+	public void setTemperature() {
+		System.out.println("tempeature is set...");
+	}
+}
