4d49e51ad7fa3192d9de25074adf1047bdc28aac
Ravi A <89862619+ravia-10@users.noreply.github.com>
2021-09-24 07:10:10 +0530

    Add files via upload

---
+++ PowerBank.java
@@ -0,0 +1,22 @@
+package com.xworkz.oops;
+
+public class PowerBank {
+	int price;
+	String color;
+	String brand;
+	double weight;
+	double height;
+	int mahPower;
+
+	public void powerBankOn() {
+		System.out.println("it is On");
+	}
+
+	public void powerBankOff() {
+		System.out.println("it is Off");
+	}
+
+	public void chargeMobile() {
+		System.out.println("it is charging");
+	}
+}
